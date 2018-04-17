7add86d7cc87361f4fb08647d5afda8b329095f5
Jeremy Woffinden <jeremywoffinden25@gmail.com>
2018-04-17 04:09:28 -1000

    working and tested 80 loops

--- main.py
+++ main.py
@@ -4,9 +4,8 @@ import time
 import keys
 import bs4
 
-# initialize my variables
-questions = []
-answers = []
+#all questions and answers
+all_questions = []
 
 # LOG IN TO CANVAS, ACCESS QUIZ PAGE
 browser = webdriver.Firefox()
@@ -23,34 +22,46 @@ time.sleep(1)
 browser.find_element_by_css_selector("input").submit()
 time.sleep(5)
 
-for x in range(60):
-    # START QUIZ
-    print "--- TAKING QUIZ ---"
-    browser.find_element_by_id("take_quiz_link").click()
-    time.sleep(1)
-    browser.find_element_by_id("submit_quiz_button").click()
-    alert = browser.switch_to.alert
-    alert.accept()
-    time.sleep(5)
-
-    # PARSE QUIZ 
-    print "--- PARSING ANSWERS ---"
-    html = browser.page_source
-    soup = bs4.BeautifulSoup(html, "html.parser")
-    for div in soup.findAll("div", {"class": "question_text user_content enhanced"}):
-        if div.find('p').text not in questions: 
-            questions.append(div.find('p').text)
-    for div in soup.findAll("div", {"class": "answer answer_for_ correct_answer"}):
-        if div.get("title") not in answers:
-            answers.append(div.get("title"))
-    time.sleep(2)
-    #print questions
-    #print answers
-    print "--- STARTING OVER ---"
-
-#print soup.prettify()
 with open("data.csv", 'w') as f:
-    for i in range(len(questions)):
-        f.write((questions[i] + "," + answers[i].split(". ")[0]).strip().encode('utf-8'))
-        f.write("\n")
-#browser.close()
+    for x in range(80):
+        questions = []
+        answers = []
+        # START QUIZ
+        print "--- TAKING QUIZ ---"
+        browser.find_element_by_id("take_quiz_link").click()
+        time.sleep(1)
+        browser.find_element_by_id("submit_quiz_button").click()
+        alert = browser.switch_to.alert
+        alert.accept()
+        time.sleep(10)
+
+        # PARSE QUIZ 
+        print "--- PARSING ANSWERS ---"
+        html = browser.page_source
+        soup = bs4.BeautifulSoup(html, "html.parser")
+        flag = []
+        flag_count = 0
+        for div in soup.findAll("div", {"class": "question_text user_content enhanced"}):
+            if div.find('p').text not in all_questions: 
+                flag.append(1)
+                all_questions.append(div.find('p').text)
+                questions.append(div.find('p').text)
+            else:
+                flag.append(0)
+        print flag
+        print flag_count
+        for div in soup.findAll("div", {"class": "answer answer_for_ correct_answer"}):
+            if flag_count < 5:    
+                if flag[flag_count] == 1:
+                    answers.append(div.get("title"))
+            flag_count+=1
+        print "--- WRITING DATA ---"
+        print questions
+        print answers
+        for i in range(len(questions)):
+            f.write((questions[i] + ";" + answers[i].replace("This was the correct answer.", '').strip()).encode('utf-8'))
+            f.write("\n")
+        time.sleep(5)
+        #print questions
+        #print answers
+        print "--- LOOP: " + str(x+1) + " OVER ---"
